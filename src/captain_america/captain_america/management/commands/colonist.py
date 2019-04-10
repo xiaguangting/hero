9de8f46a89d95933839289f55e3bde633f40b253
xiakang <xiakang@fun.tv>
2019-04-10 10:33:12 +0800

    update

--- src/captain_america/captain_america/management/commands/colonist.py
+++ src/captain_america/captain_america/management/commands/colonist.py
@@ -16,6 +16,7 @@ from spider.models import Control
 
 class Command(BaseCommand):
     help = 'Run spiders by Control'
+    spider_script_path = os.path.join(os.path.dirname(os.getcwd()), SPIDER_PROJECT_NAME, SPIDER_PROJECT_SCRIPT)
 
     # def add_arguments(self, parser):
     #     parser.add_argument('poll_id', nargs='+', type=int)
@@ -23,7 +24,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # 扫描控制表，更新定时任务
         interval = COLONIST_INTERVAL
-        spider_script_path = os.path.join(os.path.dirname(os.getcwd()), SPIDER_PROJECT_NAME, SPIDER_PROJECT_SCRIPT)
         while True:
             my_user_cron = CronTab(user=True)
 
@@ -34,30 +34,30 @@ class Command(BaseCommand):
             for i in crontab_id_set - control_id_set:  # 删除Crontab
                 crontab_id_map[i].delete()
             for i in control_id_set - crontab_id_set:  # 增加Crontab
-                control = control_id_map[i]
-                command = '%s %s' % (spider_script_path, i.code)
-                comment = COLONIST_CRONTAB_COMMENT.format(i.id)
-                self.update_crontab(my_user_cron.new(command), comment, control.cycle, control.is_disabled)
+                self.add_crontab(my_user_cron, control_id_map[i])
 
             # 定时更新Crontab
             offset_time = datetime.datetime.now() - datetime.timedelta(seconds=interval)
             control_qs = Control.objects.filter(is_disabled=False, update_time__gt=offset_time)
             for i in control_qs:
-                command = '%s %s' % (spider_script_path, i.code)
-                comment = COLONIST_CRONTAB_COMMENT.format(i.id)
                 is_exist = False
-                for j in my_user_cron.find_comment(comment):
-                    self.update_crontab(j, comment, i.cycle, i.is_disabled, command)
+                for j in my_user_cron.find_comment(COLONIST_CRONTAB_COMMENT.format(i.id)):
+                    self.alter_crontab(j, i)
                     is_exist = True
                 if not is_exist:
-                    self.update_crontab(my_user_cron.new(command), comment, i.cycle, i.is_disabled)
+                    self.add_crontab(my_user_cron, i)
 
             my_user_cron.write()  # 写入Crontab操作
             time.sleep(interval)
 
-    def update_crontab(self, job, comment, cycle, is_disabled, command=None):
-        job.setcomment(comment)
-        job.setall(cycle)
-        job.enable(not is_disabled)
-        if command is not None:
-            job.set_command(command)
+    def alter_crontab(self, job, model_object):
+        job.set_command('%s %s' % (self.spider_script_path, model_object.code))
+        job.setall(model_object.cycle)
+        job.enable(not model_object.is_disabled)
+
+    def add_crontab(self, crontab, model_object):
+        command = '%s %s' % (self.spider_script_path, model_object.code)
+        comment = COLONIST_CRONTAB_COMMENT.format(model_object.id)
+        job = crontab.new(command, comment)
+        job.setall(model_object.cycle)
+        job.enable(not model_object.is_disabled)
