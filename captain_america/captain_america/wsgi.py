f36fb98ec281948440ba4fce5a7f27cf3baa86ae
xiakang <xiakang@fun.tv>
2019-04-12 17:27:13 +0800

    u

--- captain_america/captain_america/wsgi.py
+++ captain_america/captain_america/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "captain_america.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "captain_america.settings_pro")
 
 application = get_wsgi_application()
